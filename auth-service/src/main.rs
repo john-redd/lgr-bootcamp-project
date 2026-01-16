d4a255026e0312f4f3c5cb47940e4a79ddd23808
John Redd <john.redd@endevvr.io>
2026-01-16 15:04:57 -0700

    use imported function. add no-tests flag to nextest.

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -14,7 +14,7 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
 
-    axum::serve(listener, app).await.unwrap();
+    serve(listener, app).await.unwrap();
 }
 
 async fn health_handler() -> impl IntoResponse {
