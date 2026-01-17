05d2e12da6da561f0bd7f2915395b39f26da2c9f
John Redd <john.redd@endevvr.io>
2026-01-16 18:13:56 -0700

    fix fmt issues.

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -3,4 +3,3 @@ use axum::{http::StatusCode, response::IntoResponse};
 pub async fn post_signup() -> impl IntoResponse {
     StatusCode::OK
 }
-
--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -3,4 +3,3 @@ use axum::{http::StatusCode, response::IntoResponse};
 pub async fn post_verify_2fa() -> impl IntoResponse {
     StatusCode::OK
 }
-
