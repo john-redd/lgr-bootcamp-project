7a284ae1c48544869a6a1999ff722416baa4f410
John Redd <john.redd@endevvr.io>
2026-01-16 17:56:23 -0700

    add tests for other routes.

--- auth-service/tests/api/routes.rs
+++ auth-service/tests/api/routes.rs
@@ -1,11 +1,67 @@
-use crate::helpers::run_app;
+use crate::helpers::TestApp;
+use reqwest::StatusCode;
 
 #[tokio::test]
-async fn test_app_starts() {
-    let result = run_app().await;
+async fn test_health_endpoint_returns_200() {
+    let test_app = TestApp::build().await.expect("failed to start test app");
 
-    assert!(result.is_ok());
-    if let Ok(address) = result {
-        assert!(!address.is_empty())
-    };
+    let response = test_app.get_heatlh().await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+    assert_eq!(response.text().await.unwrap(), "OK");
+}
+
+#[tokio::test]
+async fn test_root_endpoint_returns_200() {
+    let test_app = TestApp::build().await.expect("failed to start test app");
+
+    let response = test_app.get_root().await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+    assert_eq!(response.headers().get("Content-Type").unwrap(), "text/html");
+}
+
+#[tokio::test]
+async fn test_signup_endpoint_returns_200() {
+    let test_app = TestApp::build().await.expect("failed to start test app");
+
+    let response = test_app.post_signup().await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn test_login_endpoint_returns_200() {
+    let test_app = TestApp::build().await.expect("failed to start test app");
+
+    let response = test_app.post_login().await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn test_verify_2fa_endpoint_returns_200() {
+    let test_app = TestApp::build().await.expect("failed to start test app");
+
+    let response = test_app.post_verify_2fa().await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn test_logout_endpoint_returns_200() {
+    let test_app = TestApp::build().await.expect("failed to start test app");
+
+    let response = test_app.post_logout().await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn test_verify_token_endpoint_returns_200() {
+    let test_app = TestApp::build().await.expect("failed to start test app");
+
+    let response = test_app.post_verify_token().await;
+
+    assert_eq!(response.status(), StatusCode::OK);
 }
